3e97d1d6cb27094fcb78cc06822a65a38fd9724b
David Giraldo <david.giraldo@kwalee.com>
2026-09-21 15:01:46 +0100

    allow audio from the hub and games

--- games/hub-reward.js
+++ games/hub-reward.js
@@ -1,5 +1,49 @@
 (function (w) {
   "use strict";
+
+  var NativeAC = w.AudioContext || w.webkitAudioContext;
+  var contexts = [];
+  if (NativeAC && !NativeAC.__kwaleeWrapped) {
+    function WrappedAudioContext(opts) {
+      var ctx = new NativeAC(opts);
+      contexts.push(ctx);
+      return ctx;
+    }
+    WrappedAudioContext.prototype = NativeAC.prototype;
+    WrappedAudioContext.__kwaleeWrapped = true;
+    w.AudioContext = WrappedAudioContext;
+    if ("webkitAudioContext" in w) w.webkitAudioContext = WrappedAudioContext;
+  }
+
+  function unlockAudio() {
+    var i;
+    for (i = 0; i < contexts.length; i++) {
+      try {
+        if (contexts[i] && contexts[i].state === "suspended") contexts[i].resume();
+      } catch (err) { /* ignore */ }
+    }
+    try {
+      var media = w.document && w.document.querySelectorAll("audio, video");
+      if (media) {
+        for (i = 0; i < media.length; i++) {
+          var el = media[i];
+          if (el.paused) el.play().catch(function () { /* autoplay still blocked */ });
+        }
+      }
+    } catch (err) { /* ignore */ }
+  }
+  w.KwaleeUnlockAudio = unlockAudio;
+
+  w.addEventListener("message", function (e) {
+    var data = e.data;
+    if (data && data.type === "kwalee.unlockAudio") unlockAudio();
+  });
+  if (w.document) {
+    w.document.addEventListener("pointerdown", unlockAudio, true);
+    w.document.addEventListener("touchstart", unlockAudio, true);
+    w.document.addEventListener("click", unlockAudio, true);
+  }
+
   var last = 0;
   function send() {
     if (w.parent === w) return;
